9f650edc8d198b80e409a0e790521e5c2c89f370
Kevin J Dolan <kevin.dolan@kjdElectronics.com>
2020-02-27 12:44:31 -0500

    Mic demo

--- bin/mic-demo.js
+++ bin/mic-demo.js
@@ -25,14 +25,31 @@ function main() {
                exitOnSilence: 6
            });
            const micInputStream = micInstance.getAudioStream();
-           micInputStream.pipe(client.connection.inputStream());
 
+           let isPipedToMumble = false;
            micInputStream.on('data', function(data) {
                console.log("Mic Input Stream Data: " + data.length);
+               if(!isPipedToMumble) {
+                   isPipedToMumble = true; //Set flag to true. If no silence signal is received pipe will restart
+                   setTimeout(() => {
+                       if(isPipedToMumble){
+                           micInputStream.pipe(client.connection.inputStream());
+                           log.info(`Beginning Audio Stream`);
+                       }
+                   }, 10);
+               }
+           });
+
+           micInputStream.on('error', function(err) {
+               console.log("Error in Input Stream: " + err);
            });
 
            micInputStream.on('silence', function() {
                console.log("Got SIGNAL silence");
+               if(isPipedToMumble) {
+                   micInputStream.unpipe(); //Stop sending data to mumble client
+                   isPipedToMumble = false;
+               }
            });
 
            micInstance.start();
