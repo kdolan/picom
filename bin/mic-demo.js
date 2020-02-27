97e95ee0f027d54a75654da13d9fd0f0f7e09983
Kevin J Dolan <kevin.dolan@kjdElectronics.com>
2020-02-27 14:15:27 -0500

    Mic demo

--- bin/mic-demo.js
+++ bin/mic-demo.js
@@ -3,12 +3,73 @@ const Mic = require('mic');
 const MumbleClientService = require('../service/MumbleClientService').MumbleClientService;
 const Gpio = require('../service/Gpio').Gpio;
 
+const CALL_BTN_PIN=20;
+const TX_BTN_PIN=21;
+const CALL_LED_PIN=26;
+const GLITH_FILTER=1000;
+
 const log = require('loglevel');
 if(process.env.NODE_ENV !== "production")
     log.setDefaultLevel("debug");
 
 
 const CONFIG = require('../config/mumble/local');
+
+function setupTxButton({micInstance}) {
+    const txButton = new Gpio(TX_BTN_PIN, {
+        mode: Gpio.INPUT,
+        pullUpDown: Gpio.PUD_UP,
+        alert: true
+    });
+
+    if(txButton.glitchFilter)
+        txButton.glitchFilter(GLITH_FILTER);
+    else
+        log.warn(`WARNING - No mocked glitchFilter`);
+
+    txButton.on('alert', (level, tick) => {
+        if(!level) {
+            log.info('Tx Button Pressed');
+            micInstance.resume();
+        }
+        else {
+            micInstance.pause();
+            log.info('Tx Button Released');
+        }
+    });
+}
+
+function setupCallButton({led, sendMessageFn}) {
+    const callButton = new Gpio(CALL_BTN_PIN, {
+        mode: Gpio.INPUT,
+        pullUpDown: Gpio.PUD_UP,
+        alert: true
+    });
+
+    if(callButton.glitchFilter)
+        callButton.glitchFilter(GLITH_FILTER);
+    else
+        log.warn(`WARNING - No mocked glitchFilter`);
+
+    callButton.on('alert', (level, tick) => {
+        if(!level) {
+            log.info('Call Button Pressed');
+            led.digitalWrite(1);
+            sendMessageFn("CALLING");
+        }
+        else {
+            led.digitalWrite(0);
+            log.info('Call Button Released');
+            sendMessageFn("END CALLING");
+        }
+    });
+}
+
+function setupCallLed() {
+    const led = new Gpio(CALL_LED_PIN, {mode: Gpio.OUTPUT});
+    log.debug('Call LED setup on ' + CALL_LED_PIN);
+    return led;
+}
 
 function main() {
    const client = new MumbleClientService(CONFIG);
@@ -19,28 +80,6 @@ function main() {
        .then(r => {
            client.sendMessageToCurrentChannel("Mic Client");
 
-           const button = new Gpio(21, {
-               mode: Gpio.INPUT,
-               pullUpDown: Gpio.PUD_UP,
-               alert: true
-           });
-
-           if(button.glitchFilter)
-               button.glitchFilter(1000);
-           else
-               log.warn(`WARNING - No mocked glitchFilter`);
-
-           button.on('alert', (level, tick) => {
-               if(!level) {
-                   log.info('Button Pressed');
-                   micInstance.resume();
-               }
-               else {
-                   micInstance.pause();
-                   log.info('Button Released');
-               }
-           });
-
            let micInstance = Mic({
                rate: '88000',
                channels: '1',
@@ -48,6 +87,10 @@ function main() {
                device: "hw:CARD=Device,DEV=0",
                exitOnSilence: 0
            });
+
+           const callLed = setupCallLed();
+           setupTxButton({micInstance});
+           setupCallButton({led: callLed, sendMessageFn: client.sendMessageToCurrentChannel});
 
            const micInputStream = micInstance.getAudioStream();
            let mumbleWriteStream = client.connection.inputStream();
@@ -63,6 +106,17 @@ function main() {
 
            micInputStream.on('processExitComplete', function() {
                console.log("Got SIGNAL processExitComplete");
+           });
+
+           client.on('message', ({message, user}) => {
+               if(message === "CALLING") {
+                   log.info(`Call signal received from ${user.name}`);
+                   callLed.digitalWrite(1);
+               }
+               else if(message === "END CALLING") {
+                   log.info(`Call signal ended from ${user.name}`);
+                   callLed.digitalWrite(0);
+               }
            });
 
            micInstance.start();
