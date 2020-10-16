d5625ad41925429343b7daba665ba2157dc20df8
Kevin J Dolan <kevin.dolan@kjdElectronics.com>
2020-10-16 13:27:24 -0400

    WIP Auto Config

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+echo "Removing Existing Install..."
 rm -Rf /etc/picom
 cd /etc/
+echo "Downloading..."
 git clone https://github.com/kdolan/picom
 cd picom
 rm -Rf .git
 
 #Sim Link for Startup File
+echo "Createing symlink for auto config..."
 ln -s scripts/picom.auto.config.sh /etc/init.d/
--- scripts/picom.auto.config.sh
+++ scripts/picom.auto.config.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 cd /
 chown -R pi:pi /media/usb
-mount /dev/sda1 /media/usb -o uid=pi,gid=pi
+if grep -qs '/media/usb ' /proc/mounts; then
+    echo "'/media/usb already mounted"
+else
+    echo "Mounting Drive"
+    mount /dev/sda1 /media/usb -o uid=pi,gid=pi
+fi
+
 
 node /etc/picom/bin/auto-config.js
