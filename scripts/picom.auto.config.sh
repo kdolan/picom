30254ecc43d166bab6f4053c1c821db22da4f1de
Kevin J Dolan <kevin.dolan@kjdElectronics.com>
2020-10-16 13:33:52 -0400

    WIP Auto Config

--- scripts/picom.auto.config.sh
+++ scripts/picom.auto.config.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 cd /
 chown -R pi:pi /media/usb
-if grep -qs '/media/usb ' /proc/mounts; then
+if cat /proc/mounts | tail -n 1 | grep -q usb0; then
     echo "'/media/usb already mounted"
 else
     echo "Mounting Drive"
     mount /dev/sda1 /media/usb -o uid=pi,gid=pi
 fi
-
 
 node /etc/picom/bin/auto-config.js
