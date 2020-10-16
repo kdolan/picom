1b5e7cd5be4afd4c5a01687d6fb93f3792d3614e
Kevin J Dolan <kevin.dolan@kjdElectronics.com>
2020-10-16 14:09:57 -0400

    WIP Auto Config

--- scripts/picom.auto.config.sh
+++ scripts/picom.auto.config.sh
@@ -4,7 +4,7 @@ if cat /proc/mounts | tail -n 1 | grep -q usb0; then
     echo "'/media/usb already mounted"
 else
     echo "Mounting Drive"
-    mount /dev/sda1 /media/usb0 -o uid=pi,gid=pi
+    mount /dev/sda1 /mnt -o uid=pi,gid=pi
 fi
 
 echo "Running script.."
